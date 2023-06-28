5a8dc7db160ef22054c7362b8a09880aaf6e29aa
Mildred1207 <782199425@qq.com>
2023-06-28 14:44:51 +0800

    修改view文件的防止错误

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,11 @@
 // createWebHistory：创建history模式的路由
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Open from "@/components/open/index.vue"
-import Login from "@/components/login/index.vue"
-import StudentLayout from "@/components/studentLayout/index.vue"
-import TeacherLayout from "@/components/teacherLayout/index.vue"
-import AdminLayout from "@/components/teacherLayout/index.vue"
+import Open from "@/views/open/index.vue"
+import Login from "@/views/login/index.vue"
+import StudentLayout from "@/views/studentLayout/index.vue"
+import TeacherLayout from "@/views/teacherLayout/index.vue"
+import AdminLayout from "@/views/teacherLayout/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
