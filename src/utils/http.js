374e5974e9fad477b034982fa84bcd511909dbe0
SN7999 <2394713985@qq.com>
2023-06-30 09:47:18 +0800

    添加统一错误提示

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 // axios基础的封装
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
     //基地址-wmz测试
@@ -17,6 +19,11 @@ httpInstance.interceptors.request.use(config => {
 }, e => Promise.reject(e))
 //axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data,e=>{
+	//统一错误提示
+	ElMessage({
+		type: 'warning',
+		message: e.response.data.message
+	})
     return Promise.reject(e)
 })
 
