31e4b880286c63878d495e58709f24db790a3e2c
SN7999 <2394713985@qq.com>
2023-07-06 11:49:55 +0800

    成绩

--- src/views/studentLayout/component/exam/examContent.vue
+++ src/views/studentLayout/component/exam/examContent.vue
@@ -1,28 +1,26 @@
 <template>
 	<el-form ref="form" :model="formData" label-width="80px">
-	    <div v-for="(question, index) in questions" :key="question.id">
-	      <div>{{ question.question }}</div>
-	      <el-form-item :prop="'question' + question.id">
-	        <template v-if="question.options">
-	          <el-radio-group v-model="formData['question' + question.id]">
-	            <el-radio v-for="(option, optionIndex) in question.options" :key="option.id" :label="option.id">{{ option.text }}</el-radio>
-	          </el-radio-group>
-	        </template>
-	        <template v-else>
-	          <el-input type="textarea" v-model="formData['question' + question.id]"></el-input>
-	        </template>
-	      </el-form-item>
-	    </div>
-	
-	    <el-form-item>
-	      <el-button type="primary" @click="submitForm">提交</el-button>
-	    </el-form-item>
-	  </el-form>
+		<div v-for="(question, index) in questions" :key="question.id">
+			<div>{{ question.question }}</div>
+			<el-form-item :prop="'question' + question.id">
+				<template v-if="question.options">
+					<el-radio-group v-model="formData['question' + question.id]">
+						<el-radio v-for="(option, optionIndex) in question.options" :key="option.id" :label="option.id">{{ option.text }}</el-radio>
+					</el-radio-group>
+				</template>
+				<template v-else>
+					<el-input type="textarea" v-model="formData['question' + question.id]"></el-input>
+				</template>
+			</el-form-item>
+		</div>
+
+		<el-form-item><el-button type="primary" @click="submitForm">提交</el-button></el-form-item>
+	</el-form>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { ElMessage } from "element-plus";
+import { ElMessage } from 'element-plus';
 
 const formData = ref({});
 const questions = [
@@ -50,19 +48,17 @@ const questions = [
 
 const submitForm = () => {
 	for (const question of questions) {
-	    let answer;
-	    if (question.options) {
-	      const selectedOption = question.options.find(option => option.id === formData['question' + question.id]);
-	      answer = selectedOption ? selectedOption.text : '未选择';
-	    } else {
-	      answer = formData['question' + question.id] || '未填写';
-	    }
-	    console.log(`题号: ${question.id}，答案: ${answer}`);
-	  }
-  ElMessage.success('表单提交成功！');
+		let answer;
+		if (question.options) {
+			const selectedOption = question.options.find(option => option.id === formData['question' + question.id]);
+			answer = selectedOption ? selectedOption.text : '未选择';
+		} else {
+			answer = formData['question' + question.id] || '未填写';
+		}
+		console.log(`题号: ${question.id}，答案: ${answer}`);
+	}
+	ElMessage.success('表单提交成功！');
 };
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
--- src/views/studentLayout/component/studentScore.vue
+++ src/views/studentLayout/component/studentScore.vue
@@ -1,5 +1,18 @@
+<script setup>
+	import { onMounted, ref } from 'vue';
+	import { Search } from "@element-plus/icons-vue"
+	import { ElMessage } from 'element-plus';
+	const searchScore = ref('');
+</script>
+
 <template>
     <div>
-        我是学生成绩查询界面
+        <div class="searchBox">
+			<el-input v-model.trim="searchScore" class="w-50 m-2" size="large" placeholder="请输入数字或中文或字母组成的课程名" :prefix-icon="Search" @keyup.enter="getSerchScore" />
+		</div>
     </div>
 </template>
+
+<style scoped lang="scss">
+	
+</style>
