cf31108ff3170968d2827fab2c9dbaba51b9bc12
SN7999 <2394713985@qq.com>
2023-07-07 11:46:44 +0800

    查询学生信息
    主要部分已完成

---
+++ src/apis/teacherStudentAPI.js
@@ -0,0 +1,53 @@
+import request from '@/utils/http'
+
+export const getStudentAPI = () => {
+	console.log('getStudentAPI执行了')
+    return request({
+		// url: '/teacher/getSelectClass',
+		
+		// 测试用api
+        url: '/getSelectClass',
+		
+    })
+}
+
+export const getSearchStudentListByClassAPI = (classname) => {
+	console.log('getSearchStudentListByClassAPI执行了'+classname)
+    return request({
+        method:'POST',
+        url: '/teacher/getSelectClassByClassName',
+		data:{
+			classname
+		}
+    })
+}
+
+export const getSearchStudentListByStudentAPI = (studentname) => {
+	console.log('getSearchStudentListByStudentAPI执行了'+studentname)
+    return request({
+        method:'POST',
+        url: '/teacher/getSelectClassByIdOrName',
+		data:{
+			studentname
+		}
+    })
+}
+
+// export const searchStudentInfoAPI = (studentid) => {
+// 	console.log('getSearchStudentListByStudentAPI执行了'+studentid)
+//     return request({
+//         method:'POST',
+//         url: '/',//待改
+// 		data:{
+// 			studentid
+// 		}
+//     })
+// }
+
+// 测试用
+export const searchStudentInfoAPI = (id) => {
+	console.log('getSearchStudentListByStudentAPI执行了'+id)
+    return request({
+        url: '/studentInfo'
+    })
+}
