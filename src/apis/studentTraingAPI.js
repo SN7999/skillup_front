40fa405c5d9e76c4b6268df10c47a4f4e343f96c
Mildred1207 <782199425@qq.com>
2023-07-11 14:57:50 +0800

    解决了蹦出问题的问题

--- src/apis/studentTraingAPI.js
+++ src/apis/studentTraingAPI.js
@@ -4,9 +4,9 @@ import request from '@/utils/http'
 //获取学生已选课程封面和名字
 //最终使用
 export const getSelectCurriculumAPI = () => {
-    return request({
-        url: '/student/showSelectClasses',
-    })
+  return request({
+    url: '/student/showSelectClasses'
+  })
 }
 
 // 测试使用
@@ -18,24 +18,23 @@ export const getSelectCurriculumAPI = () => {
 
 //搜索课程 url待改
 export const getSearchAPI = (classname) => {
-    return request({
-      url: '/classes/likeQueryClasses',
-      method:'POST',
-      params:{
-		classname
-	  }
-    })
+  return request({
+    url: '/classes/likeQueryClasses',
+    method: 'POST',
+    params: {
+      classname
+    }
+  })
 }
 
 /////trainingDetail 学生培训详情界面调用方法
 //最终
 export const getTrainingDetailAPI = (classid1) => {
-    return request({
-        method:'POST',
-        url: '/DTO/getclass',
-        data:
-            classid1
-    })
+  return request({
+    method: 'POST',
+    url: '/DTO/getclass',
+    data: classid1
+  })
 }
 //测试
 // export const getTrainingDetailAPI = () => {
@@ -46,11 +45,44 @@ export const getTrainingDetailAPI = (classid1) => {
 
 //改变章节完成状态
 export const getChapterFinishAPI = (chapterid) => {
-    return request({
-        method:'POST',
-        url: '/progress/finishChapter',
-        params:{
-            chapterid
-        }
-    })
+  return request({
+    method: 'POST',
+    url: '/progress/finishChapter',
+    params: {
+      chapterid
+    }
+  })
+}
+
+//获取视频暂停时间
+export const getRandomAPI = (length) => {
+  return request({
+    method: 'POST',
+    url: '/chapter/getRandomSecond',
+    params: {
+      length
+    }
+  })
+}
+
+//获取题目
+export const getRandomTest = (chapterid) => {
+  return request({
+    method: 'POST',
+    url: '/chapter/getRandomQuestion',
+    params: {
+      chapterid
+    }
+  })
+}
+
+//回答随机题目＋积分
+export const getRandomUp = (res) => {
+  return request({
+    method: 'POST',
+    url: '/chapter/checkAnswer',
+    params: {
+      res
+    }
+  })
 }
